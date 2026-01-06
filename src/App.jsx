f79777aa8ec2efacd0c21a2088ccb22de9d8ceeb
Jatin Singh <34009530+jatin0nf1re@users.noreply.github.com>
2026-01-06 18:34:35 +0530

    Fix Router for custom domain - remove basename and use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/tosha-bakery">
+    <Router>
       <div className="app">
         <Navbar />
         <main className="main-content">
